fa8f0d49a7e082422da10ebd9e1787e2ac4fb03e
Gudmundur D. Haraldsson <gudmdharalds@users.noreply.github.com>
2022-07-07 23:09:17 +0000

    Create popup-builder.php

---
+++ plugins/popup-builder/popup-builder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Popup Builder
+* Plugin URI: https://popup-builder.com
+* Description: The most complete popup plugin. Html, image, iframe, shortcode, video and many other popup types. Manage popup dimensions, effects, themes and more.
+* Version: 4.0.0
+* Author: Sygnoos
+* Author URI: https://sygnoos.com
+* License: GPLv2
+* Text Domain:  popup-builder
+* Domain Path:  /languages/
+*/
+
