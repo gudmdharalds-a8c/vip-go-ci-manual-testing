85f6858f6bbb7b845fd7bc3060a5b7559e4db795
Gudmundur D. Haraldsson <gudmdharalds@users.noreply.github.com>
2022-10-19 17:57:20 +0000

    Create redirects.php

---
+++ themes/testing/plugins/redirects/redirects.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Test Redirects
+ * Plugin URI: https://github.com/gudmdharalds-a8c/vip-go-ci-manual-testing
+ * Description: A test plugin
+ * Version: 1.0
+ * Author: Test
+ *
+ */
+
+function test() {
+}
